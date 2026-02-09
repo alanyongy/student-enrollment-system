eff6a51ebd96a649194c3b6a8b09b34035507872
alanyongy <nosforever12@gmail.com>
2026-02-09 01:54:18 -0700

    Converted signup and login process to use DTOs Reverted change to nullable names for User entity

--- src/main/java/com/example/CourseRegistrationSystem/controller/AuthController.java
+++ src/main/java/com/example/CourseRegistrationSystem/controller/AuthController.java
@@ -1,41 +1,35 @@
 package com.example.CourseRegistrationSystem.controller;
 
-import com.example.CourseRegistrationSystem.entity.User;
-import com.example.CourseRegistrationSystem.repository.UserRepository;
-import org.springframework.security.crypto.password.PasswordEncoder;
+//import org.springframework.security.crypto.password.PasswordEncoder;
+
 import org.springframework.web.bind.annotation.*;
+
+import com.example.CourseRegistrationSystem.dto.LoginRequest;
+import com.example.CourseRegistrationSystem.dto.LoginResponse;
+import com.example.CourseRegistrationSystem.dto.SignupRequest;
+import com.example.CourseRegistrationSystem.service.AuthService;
+import org.springframework.http.ResponseEntity;
+
 
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
 
-    private final UserRepository userRepository;
-    //private final PasswordEncoder passwordEncoder;
+    private final AuthService authService;
 
-    public AuthController(UserRepository userRepository,
-                          PasswordEncoder passwordEncoder) {
-        this.userRepository = userRepository;
-        //this.passwordEncoder = passwordEncoder;
+    public AuthController(AuthService authService) {
+        this.authService = authService;
     }
 
     @PostMapping("/signup")
-    public String signup(@RequestBody User user) {
-        // if (userRepository.existsByEmail(user.getEmail())) {
-        //     return "Email already has an account";
-        // }
-
-        //user.setPassword(passwordEncoder.encode(user.getPassword()));
-        userRepository.save(user);
-        return "User registered successfully";
+    public ResponseEntity<String> signup(@RequestBody SignupRequest request) {
+        authService.signup(request);
+        return ResponseEntity.ok("User registered successfully");
     }
 
     @PostMapping("/login")
-    public String login(@RequestBody User loginRequest) {
-        User user = userRepository.findByEmail(loginRequest.getEmail())
-                .orElse(null);
-        if (user == null /*||!passwordEncoder.matches(loginRequest.getPassword(), user.getPassword())*/) {
-            return "Invalid username or password";
-        }
-        return "Login successful";
+    public ResponseEntity<LoginResponse> login(@RequestBody LoginRequest request) {
+        LoginResponse response = authService.login(request);
+        return ResponseEntity.ok(response);
     }
 }
