260a0b6b3762b4564d06e193034ff3c5e65e5e90
Kian Sieppert <kiansieppert@gmail.com>
2026-02-26 22:50:17 -0700

    added list, get, create, update, and delete methods to CourseService.

--- src/main/java/com/example/CourseRegistrationSystem/service/CourseService.java
+++ src/main/java/com/example/CourseRegistrationSystem/service/CourseService.java
@@ -2,8 +2,10 @@ package com.example.CourseRegistrationSystem.service;
 
 import com.example.CourseRegistrationSystem.entity.Course;
 import com.example.CourseRegistrationSystem.repository.CourseRepository;
-import com.example.CourseRegistrationSystem.repository.InstructorRepository;
 import com.example.CourseRegistrationSystem.dto.CourseDTO;
+
+import java.util.List;
+
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
@@ -14,35 +16,63 @@ import org.springframework.stereotype.Service;
 @Service
 public class CourseService {
     
-    private CourseRepository courseRepository;
-    private ModelMapper modelMapper;
+    private final CourseRepository courseRepository;
+    private final ModelMapper modelMapper;
      
+    public CourseService(CourseRepository courseRepository, ModelMapper modelMapper) {
+        this.courseRepository = courseRepository;
+        this.modelMapper = modelMapper;
+    }    
+
 
     // List courses
     // GET /api/admin/courses
-
+    public List<CourseDTO> listCourses() {
+        
+        return courseRepository.findAll()
+                .stream()
+                .map(i -> modelMapper.map(i, CourseDTO.class)) // map entity to dto
+                .toList();
+    }
 
 
     // Get course
     // GET /api/admin/courses/{id}
-
+    public CourseDTO getCourse(Long id) {
+        Course entity = courseRepository.findById(id)
+            .orElseThrow(() -> new RuntimeException("Course not found.")); // if id doesn't exist (handles Optional)
+        return modelMapper.map(entity, CourseDTO.class);            // map entity to dto
+    }
 
 
     // Create course
     // POST /api/admin/courses
-
-
+    public CourseDTO createCourse(CourseDTO dto) {
+        Course entity = modelMapper.map(dto, Course.class); // make entity from dto
+        Course saved = courseRepository.save(entity);                       // save entity in repo
+        return modelMapper.map(saved, CourseDTO.class);         // turn to dto again
+    }
 
 
     // Update course
     // PUT /api/admin/courses/{id}
-
-
+    public CourseDTO updateCourse(Long id, CourseDTO dto) {
+        Course entity = courseRepository.findById(id)
+            .orElseThrow(() -> new RuntimeException("Course not found.")); // ensure entity exists
+        
+        modelMapper.map(dto, entity);                                           // copy dto onto entity
+        Course updated = courseRepository.save(entity);                 // save changes
+        return modelMapper.map(updated, CourseDTO.class);   // return updated dto
+    }
 
 
     // Delete course
     // DELETE /api/admin/courses/{id}
-
+    public boolean deleteCourse(Long id) {
+        if(!courseRepository.existsById(id)) return false;  // can't find course
+        courseRepository.deleteById(id);                    // delete from db
+        return true;                    
+    }    
 
 
 }
