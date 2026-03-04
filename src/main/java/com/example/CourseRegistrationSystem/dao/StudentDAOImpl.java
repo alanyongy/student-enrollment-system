0ddc7debfa7071ff5a1bbfc23f41d513cfdd644a
Prateek <prateek.babani@ucalgary.ca>
2026-03-04 15:29:00 -0700

    Added exception and StudentDAO

---
+++ src/main/java/com/example/CourseRegistrationSystem/dao/StudentDAOImpl.java
@@ -0,0 +1,48 @@
+package com.example.CourseRegistrationSystem.dao;
+
+import com.example.CourseRegistrationSystem.entity.Student;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+import java.util.Optional;
+
+public class StudentDAOImpl implements StudentDAO {
+
+    private final EntityManager entityManager;
+
+    @Autowired
+    public StudentDAOImpl(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+
+    @Override
+    public List<Student> findAll() {
+
+        TypedQuery<Student> query =
+                entityManager.createQuery("FROM Student", Student.class);
+
+        return query.getResultList();
+    }
+
+    @Override
+    public Optional<Student> findById(Long id) {
+
+        Student student = entityManager.find(Student.class, id);
+
+        return Optional.ofNullable(student);
+    }
+
+
+    @Override
+    public Student save(Student student) {
+        return entityManager.merge(student);
+    }
+
+    @Override
+    public void delete(Student student) {
+        entityManager.remove(student);
+    }
+}
